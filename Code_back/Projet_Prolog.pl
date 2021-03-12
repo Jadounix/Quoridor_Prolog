b07e139e4723137da85b852556f346081bf08171
MarineCrlz <marine.carliez@gmail.com>
2021-03-12 11:04:41 +0100

    Recommence projet

--- Code_back/Projet_Prolog.pl
+++ Code_back/Projet_Prolog.pl
@@ -9,7 +9,7 @@
 %infosPion(Id,PointDepart,Position,LigneArrivee)
 
 %_C_Definition_Joueur
-%infosJoueur(Nom,NombrePartiesGagnees)
+%infosJoueur(Id,NombrePartiesGagnees)
 %pionPartie(pion)
 %joueur(infosJoueur(Nom,NombrePartiesGagnees),?pionPartie(pion),?NombreMursRestant)
 
@@ -23,14 +23,16 @@
 %_II)Base_de_donnees
 
 %Pion
-pion('P1',coordonnees(0,4),coordonnees(0,4),coordonnees(9,0)).
-pion('P2',coordonnees(9,4),coordonnees(9,4),coordonnees(0,9)).
-pion('P3',coordonnees(9,4),coordonnees(9,4),coordonnees(0,9)).
-pion('P4',coordonnees(9,4),coordonnees(9,4),coordonnees(0,9)).
+%infosPion(Id,PointDepart,Position,LigneArrivee)
+%pion('P1',coordonnees(0,4),coordonnees(0,4),coordonnees(9,0)).
+%pion('P2',coordonnees(9,4),coordonnees(9,4),coordonnees(0,9)).
+%pion('P3',coordonnees(9,4),coordonnees(9,4),coordonnees(0,9)).
+%pion('P4',coordonnees(9,4),coordonnees(9,4),coordonnees(0,9)).
 
-%Joueurs
-joueur('Jerome',0,pion1,10).
-joueur('Pierre-Alexandre',1,pion2,10).
+%Joueurs_noormalement_ok
+%joueur(nom,coordonnées,nbmur)
+%joueur(['J1', coordonnees(0,4), 10]).
+%joueur(['J2', coordonnees(9,4)], 10]).
 
 %Mur
 listeMur([]).
@@ -38,4 +40,24 @@ listeMur([]).
 %_III)Fonction_jeu
 
 %Lancer_Partie
+
+%On_recommence_le_code
+
+%A)Initialisation_predicats_base
+
+%Joueurs_noormalement_ok
+%joueur(nom,coordonnées,nbmur)
+
+:- dynamic(joueur/1)
+
+joueur(['J1', coordonnees(0,4), 10]).
+joueur(['J2', coordonnees(9,4)], 10]).
+
+%Etat_plateau
+%([positionJoueur1,positionJoueur2, [liste mur]])
+
+:- dynamic(plateau/1)
+
+plateau([X1,Y1],[X2,Y2],[]).
+
 
