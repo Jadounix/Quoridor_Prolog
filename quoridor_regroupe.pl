7b6d43f7e14a4eb0b0375cf758724cbba7116518
MarineCrlz <marine.carliez@gmail.com>
2021-04-07 18:48:25 +0200

    Ajout de la condition au moins un chemin possible

--- quoridor_regroupe.pl
+++ quoridor_regroupe.pl
@@ -29,6 +29,377 @@ restart :-
 %Joueur(Id,[X,Y], nbMursRestants)
 :- dynamic (joueur/3).
 
+%Enumération de tous les routes possibles entre les differents points
+:- dynamic (route/3).
+
+ 
+%déplacement horizontal (gauche droite)
+ 
+%1ère ligne
+route((1,1),(1,2), true).
+route((1,2),(1,3), true).
+route((1,3),(1,4), true).
+route((1,4),(1,5), true).
+route((1,5),(1,6), true).
+route((1,6),(1,7), true).
+route((1,7),(1,8), true).
+route((1,8),(1,9), true).
+ 
+%2e ligne
+route((2,1),(2,2), true).
+route((2,2),(2,3), true).
+route((2,3),(2,4), true).
+route((2,4),(2,5), true).
+route((2,5),(2,6), true).
+route((2,6),(2,7), true).
+route((2,7),(2,8), true).
+route((2,8),(2,9), true).
+ 
+%3e ligne
+route((3,1),(3,2), true).
+route((3,2),(3,3), true).
+route((3,3),(3,4), true).
+route((3,4),(3,5), true).
+route((3,5),(3,6), true).
+route((3,6),(3,7), true).
+route((3,7),(3,8), true).
+route((3,8),(3,9), true).
+ 
+%4e ligne
+route((4,1),(4,2), true).
+route((4,2),(4,3), true).
+route((4,3),(4,4), true).
+route((4,4),(4,5), true).
+route((4,5),(4,6), true).
+route((4,6),(4,7), true).
+route((4,7),(4,8), true).
+route((4,8),(4,9), true).
+ 
+%5e ligne
+route((5,1),(5,2), true).
+route((5,2),(5,3), true).
+route((5,3),(5,4), true).
+route((5,4),(5,5), true).
+route((5,5),(5,6), true).
+route((5,6),(5,7), true).
+route((5,7),(5,8), true).
+route((5,8),(5,9), true).
+ 
+%6e ligne
+route((6,1),(6,2), true).
+route((6,2),(6,3), true).
+route((6,3),(6,4), true).
+route((6,4),(6,5), true).
+route((6,5),(6,6), true).
+route((6,6),(6,7), true).
+route((6,7),(6,8), true).
+route((6,8),(6,9), true).
+ 
+%7e ligne
+route((7,1),(7,2), true).
+route((7,2),(7,3), true).
+route((7,3),(7,4), true).
+route((7,4),(7,5), true).
+route((7,5),(7,6), true).
+route((7,6),(7,7), true).
+route((7,7),(7,8), true).
+route((7,8),(7,9), true).
+ 
+%8e ligne
+route((8,1),(8,2), true).
+route((8,2),(8,3), true).
+route((8,3),(8,4), true).
+route((8,4),(8,5), true).
+route((8,5),(8,6), true).
+route((8,6),(8,7), true).
+route((8,7),(8,8), true).
+route((8,8),(8,9), true).
+ 
+%9e ligne
+route((9,1),(9,2), true).
+route((9,2),(9,3), true).
+route((9,3),(9,4), true).
+route((9,4),(9,5), true).
+route((9,5),(9,6), true).
+route((9,6),(9,7), true).
+route((9,7),(9,8), true).
+route((9,8),(9,9), true).
+ 
+%déplacement horizontal (droite gauche)
+ 
+%1ère ligne
+route((1,9),(1,8), true).
+route((1,8),(1,7), true).
+route((1,7),(1,6), true).
+route((1,6),(1,5), true).
+route((1,5),(1,4), true).
+route((1,4),(1,3), true).
+route((1,3),(1,2), true).
+route((1,2),(1,1), true).
+ 
+%2e ligne
+route((2,9),(2,8), true).
+route((2,8),(2,7), true).
+route((2,7),(2,6), true).
+route((2,6),(2,5), true).
+route((2,5),(2,4), true).
+route((2,4),(2,3), true).
+route((2,3),(2,2), true).
+route((2,2),(2,1), true).
+ 
+%3e ligne
+route((3,9),(3,8), true).
+route((3,8),(3,7), true).
+route((3,7),(3,6), true).
+route((3,6),(3,5), true).
+route((3,5),(3,4), true).
+route((3,4),(3,3), true).
+route((3,3),(3,2), true).
+route((3,2),(3,1), true).
+ 
+%4e ligne
+route((4,9),(4,8), true).
+route((4,8),(4,7), true).
+route((4,7),(4,6), true).
+route((4,6),(4,5), true).
+route((4,5),(4,4), true).
+route((4,4),(4,3), true).
+route((4,3),(4,2), true).
+route((4,2),(4,1), true).
+ 
+%5e ligne
+route((5,9),(5,8), true).
+route((5,8),(5,7), true).
+route((5,7),(5,6), true).
+route((5,6),(5,5), true).
+route((5,5),(5,4), true).
+route((5,4),(5,3), true).
+route((5,3),(5,2), true).
+route((5,2),(5,1), true).
+ 
+%6e ligne
+route((6,9),(6,8), true).
+route((6,8),(6,7), true).
+route((6,7),(6,6), true).
+route((6,6),(6,5), true).
+route((6,5),(6,4), true).
+route((6,4),(6,3), true).
+route((6,3),(6,2), true).
+route((6,2),(6,1), true).
+ 
+%7e ligne
+route((7,9),(7,8), true).
+route((7,8),(7,7), true).
+route((7,7),(7,6), true).
+route((7,6),(7,5), true).
+route((7,5),(7,4), true).
+route((7,4),(7,3), true).
+route((7,3),(7,2), true).
+route((7,2),(7,1), true).
+ 
+%8e ligne
+route((8,9),(8,8), true).
+route((8,8),(8,7), true).
+route((8,7),(8,6), true).
+route((8,6),(8,5), true).
+route((8,5),(8,4), true).
+route((8,4),(8,3), true).
+route((8,3),(8,2), true).
+route((8,2),(8,1), true).
+ 
+%9e ligne
+route((9,9),(9,8), true).
+route((9,8),(9,7), true).
+route((9,7),(9,6), true).
+route((9,6),(9,5), true).
+route((9,5),(9,4), true).
+route((9,4),(9,3), true).
+route((9,3),(9,2), true).
+route((9,2),(9,1), true).
+ 
+%déplacement vertical (haut vers bas)
+ 
+%1ère colonne
+route((1,1),(2,1), true).
+route((2,1),(3,1), true).
+route((3,1),(4,1), true).
+route((4,1),(5,1), true).
+route((5,1),(6,1), true).
+route((6,1),(7,1), true).
+route((7,1),(8,1), true).
+route((8,1),(9,1), true).
+ 
+%2eme colonne
+route((1,2),(2,2), true).
+route((2,2),(3,2), true).
+route((3,2),(4,2), true).
+route((4,2),(5,2), true).
+route((5,2),(6,2), true).
+route((6,2),(7,2), true).
+route((7,2),(8,2), true).
+route((8,2),(9,2), true).
+ 
+%3eme colonne
+route((1,3),(2,3), true).
+route((2,3),(3,3), true).
+route((3,3),(4,3), true).
+route((4,3),(5,3), true).
+route((5,3),(6,3), true).
+route((6,3),(7,3), true).
+route((7,3),(8,3), true).
+route((8,3),(9,3), true).
+ 
+%4eme colonne
+route((1,4),(2,4), true).
+route((2,4),(3,4), true).
+route((3,4),(4,4), true).
+route((4,4),(5,4), true).
+route((5,4),(6,4), true).
+route((6,4),(7,4), true).
+route((7,4),(8,4), true).
+route((8,4),(9,4), true).
+ 
+%5eme colonne
+route((1,5),(2,5), true).
+route((2,5),(3,5), true).
+route((3,5),(4,5), true).
+route((4,5),(5,5), true).
+route((5,5),(6,5), true).
+route((6,5),(7,5), true).
+route((7,5),(8,5), true).
+route((8,5),(9,5), true).
+ 
+%6eme colonne
+route((1,6),(2,6), true).
+route((2,6),(3,6), true).
+route((3,6),(4,6), true).
+route((4,6),(5,6), true).
+route((5,6),(6,6), true).
+route((6,6),(7,6), true).
+route((7,6),(8,6), true).
+route((8,6),(9,6), true).
+ 
+%7eme colonne
+route((1,7),(2,7), true).
+route((2,7),(3,7), true).
+route((3,7),(4,7), true).
+route((4,7),(5,7), true).
+route((5,7),(6,7), true).
+route((6,7),(7,7), true).
+route((7,7),(8,7), true).
+route((8,7),(9,7), true).
+ 
+%8eme colonne
+route((1,8),(2,8), true).
+route((2,8),(3,8), true).
+route((3,8),(4,8), true).
+route((4,8),(5,8), true).
+route((5,8),(6,8), true).
+route((6,8),(7,8), true).
+route((7,8),(8,8), true).
+route((8,8),(9,8), true).
+ 
+%9eme colonne
+route((1,9),(2,9), true).
+route((2,9),(3,9), true).
+route((3,9),(4,9), true).
+route((4,9),(5,9), true).
+route((5,9),(6,9), true).
+route((6,9),(7,9), true).
+route((7,9),(8,9), true).
+route((8,9),(9,9), true).
+ 
+%déplacement vertical (bas haut)
+ 
+%1ère colonne
+route((9,1),(8,1), true).
+route((8,1),(7,1), true).
+route((7,1),(6,1), true).
+route((6,1),(5,1), true).
+route((5,1),(4,1), true).
+route((4,1),(3,1), true).
+route((3,1),(2,1), true).
+route((2,1),(1,1), true).
+ 
+%2eme colonne
+route((9,2),(8,2), true).
+route((8,2),(7,2), true).
+route((7,2),(6,2), true).
+route((6,2),(5,2), true).
+route((5,2),(4,2), true).
+route((4,2),(3,2), true).
+route((3,2),(2,2), true).
+route((2,2),(1,2), true).
+ 
+%3eme colonne
+route((9,3),(8,3), true).
+route((8,3),(7,3), true).
+route((7,3),(6,3), true).
+route((6,3),(5,3), true).
+route((5,3),(4,3), true).
+route((4,3),(3,3), true).
+route((3,3),(2,3), true).
+route((2,3),(1,3), true).
+ 
+%4eme colonne
+route((9,4),(8,4), true).
+route((8,4),(7,4), true).
+route((7,4),(6,4), true).
+route((6,4),(5,4), true).
+route((5,4),(4,4), true).
+route((4,4),(3,4), true).
+route((3,4),(2,4), true).
+route((2,4),(1,4), true).
+ 
+%5eme colonne
+route((9,5),(8,5), true).
+route((8,5),(7,5), true).
+route((7,5),(6,5), true).
+route((6,5),(5,5), true).
+route((5,5),(4,5), true).
+route((4,5),(3,5), true).
+route((3,5),(2,5), true).
+route((2,5),(1,5), true).
+ 
+%6eme colonne
+route((9,6),(8,6), true).
+route((8,6),(7,6), true).
+route((7,6),(6,6), true).
+route((6,6),(5,6), true).
+route((5,6),(4,6), true).
+route((4,6),(3,6), true).
+route((3,6),(2,6), true).
+route((2,6),(1,6), true).
+ 
+%7eme colonne
+route((9,7),(8,7), true).
+route((8,7),(7,7), true).
+route((7,7),(6,7), true).
+route((6,7),(5,7), true).
+route((5,7),(4,7), true).
+route((4,7),(3,7), true).
+route((3,7),(2,7), true).
+route((2,7),(1,7), true).
+ 
+%8eme colonne
+route((9,8),(8,8), true).
+route((8,8),(7,8), true).
+route((7,8),(6,8), true).
+route((6,8),(5,8), true).
+route((5,8),(4,8), true).
+route((4,8),(3,8), true).
+route((3,8),(2,8), true).
+route((2,8),(1,8), true).
+ 
+%9eme colonne
+route((9,9),(8,9), true).
+route((8,9),(7,9), true).
+route((7,9),(6,9), true).
+route((6,9),(5,9), true).
+route((5,9),(4,9), true).
+route((4,9),(3,9), true).
+route((3,9),(2,9), true).
+route((2,9),(1,9), true).
 
 % libération des ressources
 liberer :-
@@ -85,19 +456,21 @@ murPerpendiculaire(X,Y) :-
      murs(h,X,Y)),
     write('Vous ne pouvez pas croiser les murs').
 
+%Verifier si un chemin existe entre deux points
+chemin(Depart, Arrivee, Chemin):-
+    chemin(Depart, Arrivee, [Depart], Chemin). % ne sert qu'à ajouter le troisième argument
+% c'est le chemin déjà parcouru, donc la ville de départ au début
 
+% si je suis à la ville d'arrivée, c'est que le travail est fini :
+% le chemin déjà parcouru est alors le chemin recherché
+chemin(Arrivee, Arrivee, Chemin, Chemin):-!.
 
-%verifier que les murs ne bloqueront pas l'acces a la ligne d'arrivee apres ajout
-% <=> ne doit pas y avoir un chemin de X=1 à X=9 via des Y ?
-/*ligneArriveeAccessible (X,Y):-
-    ( 
-    ),
-    write("Vous ne pouvez pas bloquer l'acces à la ligne d'arrivee.").*/
-
-
-
-
-
+chemin(Depart, Arrivee,CheminParcouru, CheminComplet):-
+    findall(Ville, route(Depart, Ville, true), ListeVillesAccessibles),  % construit la liste des voisines immédiates
+    member(ProchaineEtape, ListeVillesAccessibles), % choisit une étape dans cette liste
+    not(member(ProchaineEtape, CheminParcouru)), % je ne reviens pas sur mes pas
+    append( CheminParcouru,[ProchaineEtape], NouveauChemin), % j'ajoute l'étape au parcours
+    chemin(ProchaineEtape, Arrivee, NouveauChemin, CheminComplet). % récursivement jusqu'à l'arrivée
 
 
 %verifier que aucun joueur n'est sur la case X,Y
@@ -191,16 +564,45 @@ rouge(X,Y, N) :-
         joueur(_,[A,O],_),
         afficherCases(A,O),!.
 
+%mise à jour des routes
+majRoute(D, X, Y):-
+	(D == v, X1 is X+1, Y1 is Y+1, 
+	retract(route((X,Y),(X,Y1),true)), retract(route((X1,Y),(X1,Y1),true)),
+	assert(route((X,Y),(X,Y1),false)), assert(route((X1,Y),(X1,Y1),false)));
+	(D == h, X1 is X+1, Y1 is Y+1, 
+	retract(route((X,Y),(X1,Y),true)), retract(route((X,Y1),(X1,Y1),true)),
+	assert(route((X,Y),(X1,Y),false)), assert(route((X,Y1),(X1,Y1),false))).
+
+%vérifier existence d'un chemin avant la pose du mur
+cheminExisteraEncore(D, X, Y, J):-
+	joueur(J,[Xj,Yj],_),
+	(J == 2, O is 9 ; J == 1, O is 1), %on inverse pour avoir l'adversaire
+	majRoute(D, X, Y),
+	(Xj < O,
+	chemin((Xj,Yj),(O,_),Chemin);
+	Xj > O,
+	chemin((O,_),(Xj,Yj),Chemin)).
+
+	/*not(chemin((Xj,Yj),(O,_),Chemin)),
+	(D == v, X1 is X+1, Y1 is Y+1, 
+	retract(route((X,Y),(X,Y1),false)), retract(route((X1,Y),(X1,Y1),false)),
+	assert(route((X,Y),(X,Y1),true)), assert(route((X1,Y),(X1,Y1),true)));
+	(D == h, X1 is X+1, Y1 is Y+1, 
+	retract(route((X,Y),(X1,Y),false)), retract(route((X,Y1),(X1,Y1),false)),
+	assert(route((X,Y),(X1,Y),true)), assert(route((X,Y1),(X1,Y1),true))),
+	write("Poser ce mur empêchera l'adversaire d'accéder à la ligne d'arrivée."),
+	1 =:= 0 %permet de retourner false*/
 
 
 %poser un mur horizontal
 mur(h,X,Y):-
     X > 0, Y > 0, %positionne le mur dans le plateau
     X < 9, Y < 9, %ne peut pas positionner un mur depuis la 9ème case d'une ligne / colonne sinon déborde
-    not(murhOccupe(X,Y)), %si pas deja un mur en X et/ou Y
+    not(murhOccupe(X,Y)), %si pas deja un mur en X etou Y
     not(murPerpendiculaire(X,Y)),%si pas un mur perpendiculaire
     grilleBlanche,
     joueur(J,[A,O],N),
+	cheminExisteraEncore(h,X,Y,J),
     N > 0, %si le joueur a encore des murs a placer
     retract(joueur(J,[A,O],N)),
     N1 is N-1,
@@ -210,16 +612,18 @@ mur(h,X,Y):-
     assert(joueur(J,[A,O],N1)), %on update le nb de murs restants du joueur
     assert(murs(h,X,Y)), %update la liste des murs
     (J==1,joueur(2,[Ab,Or],_);J==2,joueur(1,[Ab,Or],_)),
+	majRoute(h,X,Y),
     afficherCases(Ab,Or),!.
 
-%poser un mur horizontal
+%poser un mur vertical
 mur(v,X,Y):-
     X > 0, Y > 0, %positionne le mur dans le plateau
     X < 9, Y < 9, %ne peut pas positionner un mur depuis la 9ème case d'une ligne / colonne sinon déborde
-    not(murvOccupe(X,Y)), %si pas deja un mur en X et/ou Y
+    not(murvOccupe(X,Y)), %si pas deja un mur en X et ou Y
     not(murPerpendiculaire(X,Y)),%si pas un mur perpendiculaire
     grilleBlanche,
     joueur(J,[A,O],N),
+	cheminExisteraEncore(v,X,Y,J),
     N > 0, %si le joueur a encore des murs a placer
     retract(joueur(J,[A,O],N)),
     N1 is N-1,
@@ -229,6 +633,7 @@ mur(v,X,Y):-
     assert(joueur(J,[A,O],N1)), %on update le nb de murs restants du joueur
     assert(murs(v,X,Y)), %update la liste des murs
     (J==1,joueur(2,[Ab,Or],_);J==2,joueur(1,[Ab,Or],_)),
+	majRoute(v,X,Y),
     afficherCases(Ab,Or),!.
 
 
